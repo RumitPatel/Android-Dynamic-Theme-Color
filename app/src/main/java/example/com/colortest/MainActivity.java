65d11e83c67721c0591b8c905cc221b9b939a145
RumitPatel <rumit.patel5@gmail.com>
2022-03-24 23:13:30 +0530

    Initial Project SetUp

---
+++ app/src/main/java/example/com/colortest/MainActivity.java
@@ -0,0 +1,25 @@
+package example.com.colortest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new ThemeColors(this);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(View view){
+        int red= new Random().nextInt(255);
+        int green= new Random().nextInt(255);
+        int blue= new Random().nextInt(255);
+        ThemeColors.setNewThemeColor(MainActivity.this, red, green, blue);
+    }
+}
