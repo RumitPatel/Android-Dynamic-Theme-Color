78470c5f485624eae6f7dafa2acdc4af45edbd5f
“RumitPatel” <rumit.patel5@gmail.com>
2022-12-17 21:14:05 +0530

    unused removed.

--- app/src/main/java/com/rumit/dynamic_theme_color/MainActivity.java
+++ app/src/main/java/com/rumit/dynamic_theme_color/MainActivity.java
@@ -1,25 +1,19 @@
 package com.rumit.dynamic_theme_color;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 
-import java.util.Random;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        new ThemeColors(this);
         setContentView(R.layout.activity_main);
     }
 
     public void buttonClick(View view) {
-        int red = new Random().nextInt(255);
-        int green = new Random().nextInt(255);
-        int blue = new Random().nextInt(255);
-        ThemeColors.setNewThemeColor(MainActivity.this, red, green, blue);
+
     }
 }
